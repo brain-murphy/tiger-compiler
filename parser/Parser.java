6e91c60f45a3beb111179e6a6a0759a9d96adc8d
fishlinghu <fishlinghu@gmail.com>
2016-10-04 17:52:05 -0400

    Create Parser and its interface

---
+++ parser/Parser.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Parser implements ParserInterface
+    {
+    public class server_info
+        {
+        String name;
+        String location;
+        double tariff;
+        public server_info(String in_name, String in_location)
+            {
+            tariff = -1;
+            name = in_name;
+            location = in_location;
+            }
+        }
+    
+    }
