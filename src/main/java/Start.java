f70a77087a8073e221a9af8f3eaa5c8f4bfc62e2
Brian Murphy <brianmurphy@gatech.edu>
2016-10-05 17:23:37 -0400

    reading files, with Start.main() as the entry point

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,2 +1,24 @@
+import scanner.DirectScanner;
+import scanner.Scanner;
+import util.Reader;
+
 public class Start {
+    public static void main(String[] args) {
+
+        Reader reader = new Reader();
+
+        if (args.length > 0) {
+            for (String filePath : args) {
+                String fileContents = reader.readFromFile(filePath);
+
+                Scanner scanner = new DirectScanner(fileContents);
+                // TODO parsing
+            }
+        } else {
+            String programContents = reader.readFromStandardIn();
+
+            Scanner scanner = new DirectScanner(programContents);
+            // TODO parsing
+        }
+    }
 }
