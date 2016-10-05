ffea32f45485c7ea889631bf3953608b9d9e837a
fishlinghu <fishlinghu@gmail.com>
2016-10-05 15:55:31 -0400

    Start to write rules statically into the codes

--- parser/Parser.java
+++ parser/Parser.java
@@ -4,20 +4,109 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.List;
 
 public class Parser implements ParserInterface
     {
-    public class server_info
+    private ArrayList<ArrayList<String>> listOfRules = new ArrayList<ArrayList<String>>();
+    private Map<String, Map<String, int>> parsingTable = new HashMap<String, HashMap<String, int>>();
+    private void fill_listOfRules()
         {
-        String name;
-        String location;
-        double tariff;
-        public server_info(String in_name, String in_location)
+
+        String[][] arrayOfRules = 
             {
-            tariff = -1;
-            name = in_name;
-            location = in_location;
+            {"7", "<tiger-program>", "let", "<declaration-segment>", "in", "<stat-seq>", "end"},
+            {"5", "<declaration-segment>", "<type-declaration-list>", "<var-declaration-list>", "<funct-declaration-list>"},
+            {"3", "<type-declaration-list>", "NULL"},
+            {"4", "<type-declaration-list>", "<type-declaration>", "<type-declaration-list>"},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            {"", ""},
+            }
+
+        int i = 0;
+        while (i < ) // Need the total number of rules
+            {   
+            listOfRules.add(new ArrayList<String>);
+            ++i;
             }
         }
-    
+
+    private void fill_parsingTable()
+        {
+
+        }
+
+    public Parser(String fileText) 
+        {
+        fill_listOfRules();
+        fill_parsingTable();
+        this.fileText = fileText;
+        scannedTokens = new ArrayList<>();
+        }
     }
---
+++ parser/Symbol.java
@@ -0,0 +1,14 @@
+package buffer;
+
+class Symbol
+	{
+	private int symbolType; // 0 stands for NULL, 1 stands for T, 2 stands for NT
+	private String text;
+
+	public Symbol(int symbolType, String text)
+		{
+		this.symbolType = symbolType;
+		this.text = text;
+		}
+
+	}
