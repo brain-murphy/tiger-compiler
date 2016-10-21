ae4364be75f2a889e111d6c08a31adaa9fbb83d9
Brian Murphy <Brian Murphy>
2016-10-21 16:28:40 -0400

    added createChildScope() to SymbolTable

--- src/main/java/symantic/symboltable/SymbolTable.java
+++ src/main/java/symantic/symboltable/SymbolTable.java
@@ -4,6 +4,7 @@ package symantic.symboltable;
  * Created by Brian on 10/20/2016.
  */
 public interface SymbolTable {
+
     SymbolTable getParentScope();
 
     SymbolTable getChildScope(Symbol symbolDefiningChildScope);
@@ -11,4 +12,6 @@ public interface SymbolTable {
     Symbol lookup(String name);
 
     void insert(Symbol symbol);
+
+    SymbolTable createChildScope(Symbol symbolDefiningChildScope);
 }
