e8283cd56bbd68e7b6e6ae1725c8517713bd4855
fishlinghu <fishlinghu@gmail.com>
2016-12-02 22:44:57 -0500

    Finish naive method, bug-free, correct result

--- src/main/java/codeGeneration/RegAlloc/RegAlloc.java
+++ src/main/java/codeGeneration/RegAlloc/RegAlloc.java
@@ -9,6 +9,7 @@ import util.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by fishlinghu on 2016/11/30.
@@ -49,24 +50,55 @@ public class RegAlloc {
         ArrayList<String> listOfIR = new ArrayList<String>(Arrays.asList(tempArr)); // Split IR line by line
 
         int i = 0;
-        while(i < listOfIR.size()){
-            originalIR.add( new ArrayList<String>( Arrays.asList(listOfIR.get(i).split(", ")) ) );
-            System.out.print(" ---");
-            System.out.print( originalIR.get(i).get(0) );
-            System.out.print("\n");
+        while(i < listOfIR.size()) {
+            originalIR.add(new ArrayList<String>(Arrays.asList(listOfIR.get(i).split(", "))));
+            //System.out.print(" ---");
+            //System.out.print( originalIR.get(i).get(0) );
+            //System.out.print("\n");
             i = i + 1;
         }
+        genRegAllocNaive();
+        print_IRNaive();
     }
     public void genRegAllocNaive(){
         // naive method for generating reg allocation code
         int i = 0;
         int j, k;
-        String tempStr;
-        List<String> originalVar = new ArrayList<String>();
+        int inst_size;
+        boolean callFlag, callrFlag, branchFlag;
+        String tempStr, opStr;
+
         while(i < originalIR.size()){
             // Count the total number of registers we need for this line
-            j = 1; // originalIR.get(0) is the operation string
-            while(j < originalIR.get(i).size()) {
+            List<String> originalVar = new ArrayList<String>();
+            inst_size = originalIR.get(i).size();
+            opStr = originalIR.get(i).get(0);
+            callrFlag = false;
+            callFlag = false;
+            branchFlag = false;
+            j = 1;// originalIR.get(0) is the operation string
+            if(Objects.equals(opStr, "goto")) {
+                // skip the while loop
+                tempStr = originalIR.get(i).get(0) + ", " + originalIR.get(i).get(1);
+                outputIRNaive.add(tempStr);
+                continue;
+            }
+            else if(Objects.equals(opStr, "call")){
+                callFlag = true;
+                j = 2; // skip the first 2 strings
+            }
+            else if(Objects.equals(opStr, "callr")){
+                // ignore the third string
+                callrFlag = true;
+            }
+            else if(opStr.charAt(0) == 'b'){
+                // branch instruction, ignore the last one
+                branchFlag = true;
+                inst_size = inst_size - 1;
+            }
+            while(j < inst_size) {
+                if (callrFlag == true && j == 2)
+                    j = j + 1;
                 tempStr = originalIR.get(i).get(j);
                 if(!isNumeric( tempStr )){
                     // should consider the case b__, call, callr, goto,
@@ -86,9 +118,18 @@ public class RegAlloc {
             j = 1;
             tempStr = originalIR.get(i).get(0);
             while(j < originalIR.get(i).size()){
-                if(!isNumeric( originalIR.get(i).get(j) ){
+                if(callFlag && j == 1){
+                    tempStr = tempStr + ", " + originalIR.get(i).get(j);
+                }
+                else if(callrFlag && j == 2){
+                    tempStr = tempStr + ", " + originalIR.get(i).get(j);
+                }
+                else if(branchFlag && j == originalIR.get(i).size()-1){
+                    tempStr = tempStr + ", " + originalIR.get(i).get(j);
+                }
+                else if(!isNumeric( originalIR.get(i).get(j) )){
                     tempStr = tempStr + ", $t" + k;
-                    k = k + 1
+                    k = k + 1;
                 }
                 else{
                     tempStr = tempStr + ", " + originalIR.get(i).get(j);
@@ -103,6 +144,15 @@ public class RegAlloc {
                 outputIRNaive.add( tempStr );
                 j = j + 1;
             }
+            i = i + 1;
+        }
+    }
+    public void print_IRNaive(){
+        int i = 0;
+        System.out.print( outputIRNaive.size() );
+        while(i < outputIRNaive.size()){
+            System.out.print( outputIRNaive.get(i) );
+            System.out.print("\n");
             i = i + 1;
         }
     }
