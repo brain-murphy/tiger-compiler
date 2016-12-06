0e28871759786c777daf2b49ca26885311a76f20
Brian Murphy <Brian Murphy>
2016-12-06 11:50:07 -0500

    committing to pull most recend changes

--- src/main/java/codeGeneration/instructionSelection/StackInstructionSelector.kt
+++ src/main/java/codeGeneration/instructionSelection/StackInstructionSelector.kt
@@ -1,12 +1,31 @@
 package codeGeneration.instructionSelection
 
+import codeGeneration.instructionSelection.mips.MipsInstruction
+import codeGeneration.instructionSelection.mips.MipsOpcode
+import parser.semantic.ir.FunctionExpressionType
+import parser.semantic.ir.IntegerExpressionType
 import parser.semantic.ir.IrCode
+import parser.semantic.symboltable.Attribute
 import parser.semantic.symboltable.Symbol
 
 
 class StackInstructionSelector(private val globalVariables: List<String>) {
 
     fun makeStackFrame(functionSymbol: Symbol, code: List<IrCode>) {
+        val instructions = mutableListOf<MipsInstruction>()
 
+        setFramePointer(instructions)
+
+        val functionType = functionSymbol.getAttribute(Attribute.TYPE) as FunctionExpressionType
+
+        for (argument in functionType.params) {
+            if (argument is IntegerExpressionType) {
+
+            }
+        }
+    }
+
+    private fun setFramePointer(instructions: MutableList<MipsInstruction>) {
+        val fpAssign = MipsInstruction(MipsOpcode.add, "\$fp", )
     }
 }
