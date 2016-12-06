b7b1194ee8fe46594a8d5b85617ee27803da9e22
Brian Murphy <Brian Murphy>
2016-12-05 22:46:16 -0500

    deleted stub file and added InstructionSelector kotlin class

---
+++ src/main/java/codeGeneration/instructionSelection/InstructionSelector.kt
@@ -0,0 +1,22 @@
+package codeGeneration.instructionSelection
+
+import codeGeneration.instructionSelection.mips.MipsFormatter
+import parser.semantic.ir.FunctionCallCode
+import parser.semantic.ir.LinearIr
+import parser.semantic.ir.ThreeAddressCode
+import parser.semantic.symboltable.SymbolTable
+
+
+class InstructionSelector(private val ir: LinearIr, private val symbolTable: SymbolTable) {
+
+    private val formatter = MipsFormatter()
+
+    fun run() {
+        val usedFunctionSymbols = ir.filterIsInstance<FunctionCallCode>()
+                .map { it.functionSymbol }
+                .distinctBy { it.name }
+
+        symbolTable.getChildScope()
+
+    }
+}
